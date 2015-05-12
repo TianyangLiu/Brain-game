08687a596e73055526f0da09d3fd80a6a7dacb24
Tianyng Liu <lty0711@qq.com>
2015-05-13 06:09:14 +0800

    new version by using html, css, and jquery

---
+++ js/game.js
@@ -0,0 +1,55 @@
+// start screen
+$('#playBtn').click( function() {
+	$('#start_screen').hide();
+	$('#level_selection').show();
+});
+
+
+// instruction page one
+$('#instructionBtn').click( function() {
+	$('#start_screen').hide();
+	$('#instructionOne').show();
+});
+
+
+// start button on instruction page one
+$('.startGame').click( function() {
+	$('#instructionOne').hide();
+	$('#instructionTwo').hide();
+	$('#instructionThree').hide();
+	$('#level_selection').show();
+});
+
+
+// right arrow button on instruction page one
+$('#rightArrow1').click( function() {
+	$('#instructionOne').hide();
+	$('#instructionTwo').show();
+});
+
+
+// right arrow button on instruction page two
+$('#rightArrow2').click( function() {
+	$('#instructionTwo').hide();
+	$('#instructionThree').show();
+});
+
+$('#leftArrow').click( function() {
+	$('#instructionTwo').hide();
+	$('#instructionOne').show();
+});
+
+
+$('#leftArrow3').click( function() {
+	$('#instructionThree').hide();
+	$('#instructionTwo').show();
+});
+
+
+// level seletion page to game page
+$('#levelOneBtn').click( function() {
+	$('#level_selection').hide();
+	$('#gameScreen').show();
+});
+
+
