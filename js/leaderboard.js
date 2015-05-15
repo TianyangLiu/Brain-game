915572a9edcac2683f4a384c91b4c60dc1a84f05
XavierLau <xavierlau1@gmail.com>
2015-05-15 02:15:55 -0700

    fixed leaderboard again

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,4 +1,4 @@
-$.ajax({ url: "https://api.mongolab.com/api/1/databases/comp2910/collections/leaderboard?f={%27_id%27:0}f={%27username%27:1,%27score%27:1}s={%27score%27:1}&l=10&apiKey=m62xZhBH7kLiSw-6xFKTS87LCi1QQ3lJ",
+$.ajax({ url: "https://api.mongolab.com/api/1/databases/comp2910/collections/leaderboard?f={%27_id%27:0}&f={%27username%27:1,%27score%27:1}&s={%27score%27:1}&l=10&apiKey=m62xZhBH7kLiSw-6xFKTS87LCi1QQ3lJ",
 	type: "GET",
 	contentType: "application/json",
 	success: function(data) {
