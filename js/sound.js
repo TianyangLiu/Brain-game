ee4ccb8b57e97a82d00668851fe7ae24c0a69f06
Jiaqi Song <sjqmp3@gmail.com>
2015-05-15 16:31:07 -0700

    Jackie: timer

---
+++ js/sound.js
@@ -0,0 +1,5 @@
+var HitWall, HitStone;
+HitWall = new Audio();
+HitWall.src="sounds/HitWall.mp3"
+HitStone = new Audio();
+HitStone.src="sounds/HitStone.wav"
---
+++ js/timer.js
@@ -0,0 +1,15 @@
+var i, timerstop, divide;
+	i =0;
+	divide = 1;
+	function start(){
+		timerstop = self.setInterval("increment()",(1000/divide))
+	}
+	function increment(){
+		i++;
+		document.getElementById("timer_out").innerHTML=(i/divide);
+	}
+	function stoptime(){
+            
+		clearInterval(timerstop);
+		timerstop = null;
+	}
