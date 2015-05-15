ddc298257333e75292c5c0b8fc70da1921333772
XavierLau <xavierlau1@gmail.com>
2015-05-15 01:00:23 -0700

    Added leaderboard js. Added input form for username. Added leaderboard.

---
+++ js/leaderboard.js
@@ -0,0 +1,26 @@
+$.ajax({ url: "https://api.mongolab.com/api/1/databases/comp2910/collections/leaderboard?f={%27_id%27:0}&s={%27score%27:1}&l=10&apiKey=m62xZhBH7kLiSw-6xFKTS87LCi1QQ3lJ",
+	type: "GET",
+	contentType: "application/json",
+	success: function(data) {
+		var test = data;
+		var tableRows = "<table id='leaderboardContainer'><tr><th>Name</th><th>Score</th>";
+		for (var i = 0; i < test.length; i++) {
+			var object = test[i];
+			tableRows += "<tr><td>" + object.username + "</td><td>" +object.score + "</td></tr>";
+		}
+		tableRows += "<img src='images/leaderboard.png'></img></tr></table>";
+		document.getElementById('leaderboardScroll').innerHTML = tableRows;
+		
+	}
+});
+
+function sendData() {
+	var user = $("#userId").val();
+	var score;
+	$.ajax({ url: "https://api.mongolab.com/api/1/databases/comp2910/collections/leaderboard?apiKey=m62xZhBH7kLiSw-6xFKTS87LCi1QQ3lJ",
+		data: JSON.stringify({"username" : user}),
+		type: "POST",
+		contentType: "application/json"
+	});
+	return false;
+}
